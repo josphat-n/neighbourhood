f3cc181b3e2a703174d3665f2f52ab7e3bca6f99
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-17 15:04:34 +0300

    define a url and a view for the user's profile

--- hood/urls.py
+++ hood/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path, include
-from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
    path('', views.home, name ='hood-home'),
-   path('accounts/', include('registration.backends.simple.urls')),
-   path('login/', LoginView.as_view(template_name= '/'), name='user_login'),
-   path('logout/', LogoutView.as_view(template_name='/'), name="user_logout"),
+   path('profile/', views.profile, name='new-profile'),
 ]
