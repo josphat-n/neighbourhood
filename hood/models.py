f56ef2e11fa6065aca3bd274a01fa645d2d0d4af
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-18 21:53:03 +0300

    create a post model

--- hood/models.py
+++ hood/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Hood(models.Model):
@@ -16,3 +17,12 @@ class Business(models.Model):
    name = models.CharField(max_length =30)
    hood = models.ForeignKey(Hood,on_delete=models.CASCADE, default = 1)
    biz_email =  models.CharField(max_length =30)
+   
+class Post(models.Model):
+   title = models.CharField(max_length=100)
+   content = models.TextField()
+   date_posted = models.DateTimeField(default=timezone.now)
+   author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+   def __str__(self):
+      return self.title   
