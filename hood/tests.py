a9f5ce368557f58586519ed338cb38797f2035d5
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-20 09:29:09 +0300

    write tests for Hood class methods

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Hood, Business, Admin
 
 # Create your tests here.
+class HoodTestClass(TestCase):
+   # Setup Method
+   def setUp(self):
+      self.hood_one=Hood(name= 'Muthaiga', admin_id=1, location = 'Kiambu', occupants_count = 1)
+   
+   # Testing Instance
+   def test_instance(self):
+      self.assertTrue(isinstance(self.hood_one,Hood))     
+      
+   # Testing Save Method
+   def test_create_method(self):
+      self.hood_one.create_hood()
+      hood1 = Hood.objects.all()
+      self.assertTrue(len(hood1) > 0)    
+      
+   # Teardown Method
+   def tearDown(self):
+      Hood.objects.all().delete()           
+   
+   #Delete Method   
+   def test_delete(self):
+      self.hood_one.create_hood()
+      self.hood_one.delete_hood()
+      hood1 = Hood.objects.all()
+      self.assertTrue(len(hood1)<1)    
+      
